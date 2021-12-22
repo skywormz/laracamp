509b01dfe5165c227a73a0febf2c3e280110f952
Azis M Iqbal <fennec.project@gmail.com>
2021-12-22 21:57:52 +0700

    finish migration Camp, Camp_benefit, Checkout

---
+++ database/migrations/2021_12_22_134251_create_camp_benefits_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('camp_id')->unsigned(); // field foreign_key
+            $table->string('name');
+            $table->timestamps();
+
+            # Mapping Foreign Key
+            $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
